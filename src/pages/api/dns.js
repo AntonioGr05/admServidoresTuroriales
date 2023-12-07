7763d750455cd298aa93a250562b7e28b462373d
AntonioGr05 <agalvanrojas05@gmail.com>
2023-12-07 14:32:41 -0600

    moviminryo

--- src/pages/api/dns.js
+++ src/pages/api/dns.js
@@ -1,7 +1,7 @@
 import fs from 'fs'
 
 export default function handler(req, res) {
-    const file = "src/pages/post/prueba.md"
+    const file = "public/posts/prueba.md"
     const content = fs.readFileSync(file, 'utf8')
     res.send(content)
   }
