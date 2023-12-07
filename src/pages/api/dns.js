bb1716a5b36291860659b4603218f7fd74ac10b1
AntonioGr05 <agalvanrojas05@gmail.com>
2023-12-07 14:25:22 -0600

    modificacion de ubicacion posts

--- src/pages/api/dns.js
+++ src/pages/api/dns.js
@@ -1,7 +1,7 @@
 import fs from 'fs'
 
 export default function handler(req, res) {
-    const file = "posts/prueba.md"
+    const file = "src/pages/post/prueba.md"
     const content = fs.readFileSync(file, 'utf8')
     res.send(content)
   }
